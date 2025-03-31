3a77aa73067148563efc3f13bce12762c56cdc7f
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-03-31 10:15:40 +0200

    a

--- common.hpp
+++ common.hpp
@@ -1,12 +1,12 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
-#include "../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h"
-#include "../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h"
-#include "../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h"
-#include "../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h"
-#include "../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h"
-#include "../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_usart.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_i2c.h"
+#include "stm32f4xx_hal_spi.h"
+#include "stm32f4xx_hal_tim.h"
+#include "stm32f4xx_hal_usart.h"
 
 #include <algorithm>
 #include <array>
@@ -21,7 +21,7 @@ namespace STM32_Utility {
     using SPIHandle = SPI_HandleTypeDef*;
     using TIMHandle = TIM_HandleTypeDef*;
     using GPIOHandle = GPIO_TypeDef*;
-    using UARTHandle = UART_HandleTypeDef*;
+    using UARTHandle = USART_HandleTypeDef*;
     using I2CHandle = I2C_HandleTypeDef*;
 
 }; // namespace STM32_Utility
